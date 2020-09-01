436506b7dbbda58018c2394fd4bf58ea75af4c28
Kyra van Grinsven <kvgrinsven@sogyo.nl>
2020-09-01 16:09:00 +0200

    Try a different approach to styling

--- MancalaFrontend/src/mancala/Play.tsx
+++ MancalaFrontend/src/mancala/Play.tsx
@@ -7,31 +7,32 @@ interface PlayProps {
     playRecess(pit: number): void;
 }
 
-const Recess = styled.button`
-	width: 3em;
-	height: 3em;
-	font-size: 2em;
-	color: white;
-    background: green;
-    border-radius: 50%;
-    margin: 5px
-`
-
 export function Play({ gameState, playRecess }: PlayProps) {
 	const p1pits = gameState.players[0].pits
 	const p2pits = gameState.players[1].pits
+	
+	const recessStyle = {
+	    fontSize: "2em",
+	    width: "2em",
+	    height: "2em",
+	    color: "white",
+	    background: "green",
+	    borderRadius: "50%",
+	    margin: 5,
+	}
 
     return <div>
         <p>{gameState.players[0].name} vs {gameState.players[1].name}</p>
         
         <div>
 	        {[...p2pits].reverse().map(pit =>
-	        	 <Recess key={pit.index} onClick={() => playRecess(pit.index)} type="button">{pit.nrOfStones}</Recess>)}
+	        	 <button style={recessStyle} key={pit.index} onClick={() => playRecess(pit.index)} type="button">{pit.nrOfStones}</button>)}
         </div>
         
         <div>
 	        {p1pits.map(pit =>
-	        	 <Recess key={pit.index} onClick={() => playRecess(pit.index)} type="button">{pit.nrOfStones}</Recess>)}
+	        	 <button style={recessStyle} key={pit.index} onClick={() => playRecess(pit.index)} type="button">{pit.nrOfStones}</button>)}
         </div>
+
     </div>
 }
