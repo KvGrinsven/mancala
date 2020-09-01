e0b8b671b7c296f8917030d31255981e667162d7
Kyra van Grinsven <kvgrinsven@sogyo.nl>
2020-09-01 09:48:21 +0200

    Implement playGet

--- MancalaAPI/src/main/java/nl/sogyo/mancala/api/MancalaGame.java
+++ MancalaAPI/src/main/java/nl/sogyo/mancala/api/MancalaGame.java
@@ -4,6 +4,7 @@
 package nl.sogyo.mancala.api;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -11,6 +12,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import nl.sogyo.mancala.domain.Mancala;
 
 /**
  * @author rvvugt
@@ -27,9 +30,14 @@ public class MancalaGame {
 			@PathParam("pit") String pit, 
 			@Context HttpServletRequest request) {
 		
-		//TODO: Implement this method!
+		HttpSession session = request.getSession();
 		
-		return null;
+		Mancala mancala = (Mancala) session.getAttribute("mancala");
+		mancala.playRecess(Integer.parseInt(pit));
+		// TODO Do I have to re-set the attribute?
+		String output = new JSONResultProcessor().createJSONResponse(mancala);
+		
+		return Response.status(200).entity(output).build();
 	}
 	
 }
