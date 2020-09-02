cf1e8882454bc2f3864f1ba579fa65bb9d25c0a1
Kyra van Grinsven <kvgrinsven@sogyo.nl>
2020-09-02 10:39:38 +0200

    Experiment some more :)

--- MancalaFrontend/src/mancala/Play.tsx
+++ MancalaFrontend/src/mancala/Play.tsx
@@ -7,12 +7,39 @@ interface PlayProps {
     playRecess(pit: number): void;
 }
 
+function Kalaha({nrOfStones}) {
+	const kalahaStyle = {
+		fontSize: "2em",
+	    width: "4em",
+	    height: "4em",
+	    color: "white",
+	    background: "red",
+	    borderRadius: "50%",
+	    margin: 5,
+	}
+	
+	return <button disabled style={kalahaStyle}>{nrOfStones}</button>
+}
+
 export function Play({ gameState, playRecess }: PlayProps) {
+	
+	const recessStyle = {
+	    fontSize: "2em",
+	    width: "2em",
+	    height: "2em",
+	    color: "white",
+	    background: "green",
+	    borderRadius: "50%",
+	    margin: 5,
+	}
+	
+	const p1pits = gameState.players[0].pits
+	const p2pits = gameState.players[1].pits
 
 	function Board() {
 			return <div>
 			<section style={{display:"inline", verticalAlign:"bottom"}}>
-	           <button disabled style={kalahaStyle}>{p2pits[6].nrOfStones}</button>
+	           <Kalaha nrOfStones={p2pits[6].nrOfStones}/>
 	        </section>
 	        
 	        <section style={{display:"inline-block", verticalAlign:"middle"}}>
@@ -25,39 +52,14 @@ export function Play({ gameState, playRecess }: PlayProps) {
 		    </section>
 		    
 		    <section style={{display:"inline", verticalAlign:"bottom"}}>
-		       <button disabled style={kalahaStyle}>{p1pits[6].nrOfStones}</button>
+		       <Kalaha nrOfStones={p1pits[6].nrOfStones}/>
 		    </section>
 	    </div>
-}
-
-	const p1pits = gameState.players[0].pits
-	const p2pits = gameState.players[1].pits
-	
-	const recessStyle = {
-	    fontSize: "2em",
-	    width: "2em",
-	    height: "2em",
-	    color: "white",
-	    background: "green",
-	    borderRadius: "50%",
-	    margin: 5,
-	}
-	
-	const kalahaStyle = {
-		fontSize: "2em",
-	    width: "4em",
-	    height: "4em",
-	    color: "white",
-	    background: "red",
-	    borderRadius: "50%",
-	    margin: 5,
 	}
 
     return <div>
         <p>{gameState.players[0].name} vs {gameState.players[1].name}</p>
         
        <Board/>
-
-
     </div>
 }
