c1e3a52c2441f78e796c2100678deaef448fc644
Kyra van Grinsven <kvgrinsven@sogyo.nl>
2020-09-02 09:29:28 +0200

    Separate Board initialization into function

--- MancalaFrontend/src/mancala/Play.tsx
+++ MancalaFrontend/src/mancala/Play.tsx
@@ -8,6 +8,28 @@ interface PlayProps {
 }
 
 export function Play({ gameState, playRecess }: PlayProps) {
+
+	function Board() {
+			return <div>
+			<section style={{display:"inline", verticalAlign:"bottom"}}>
+	           <button disabled style={kalahaStyle}>{p2pits[6].nrOfStones}</button>
+	        </section>
+	        
+	        <section style={{display:"inline-block", verticalAlign:"middle"}}>
+			        {[...p2pits].reverse().filter(p => p.index < 14).map(pit =>
+			        	 <button style={recessStyle} key={pit.index} onClick={() => playRecess(pit.index)} type="button">{pit.nrOfStones}</button>)}
+					<br/>
+			        {p1pits.filter(p => p.index < 7).map(pit =>
+			        	 <button style={recessStyle} key={pit.index} onClick={() => playRecess(pit.index)} type="button">{pit.nrOfStones}</button>)}
+	
+		    </section>
+		    
+		    <section style={{display:"inline", verticalAlign:"bottom"}}>
+		       <button disabled style={kalahaStyle}>{p1pits[6].nrOfStones}</button>
+		    </section>
+	    </div>
+}
+
 	const p1pits = gameState.players[0].pits
 	const p2pits = gameState.players[1].pits
 	
@@ -23,8 +45,8 @@ export function Play({ gameState, playRecess }: PlayProps) {
 	
 	const kalahaStyle = {
 		fontSize: "2em",
-	    width: "2em",
-	    height: "2em",
+	    width: "4em",
+	    height: "4em",
 	    color: "white",
 	    background: "red",
 	    borderRadius: "50%",
@@ -34,17 +56,8 @@ export function Play({ gameState, playRecess }: PlayProps) {
     return <div>
         <p>{gameState.players[0].name} vs {gameState.players[1].name}</p>
         
-        
-        <div>
-        	<button disabled style={kalahaStyle}>{p2pits[6].nrOfStones}</button>
-	        {[...p2pits].reverse().filter(p => p.index < 14).map(pit =>
-	        	 <button style={recessStyle} key={pit.index} onClick={() => playRecess(pit.index)} type="button">{pit.nrOfStones}</button>)}
-        </div>
-        <div>
-	        {p1pits.filter(p => p.index < 7).map(pit =>
-	        	 <button style={recessStyle} key={pit.index} onClick={() => playRecess(pit.index)} type="button">{pit.nrOfStones}</button>)}
-	       <button disabled style={kalahaStyle}>{p1pits[6].nrOfStones}</button>
-        </div>
+       <Board/>
+
 
     </div>
 }
